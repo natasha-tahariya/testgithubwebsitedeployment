4091fa57960b3760748261ff4229e0cadb4afd6d
Natasha <tahariya.natasha06@gmail.com>
2015-09-25 23:30:05 +0530

    test 1

--- TetsGitKududDeployment/Models/AccountViewModels.cs
+++ TetsGitKududDeployment/Models/AccountViewModels.cs
@@ -33,7 +33,7 @@ namespace TetsGitKududDeployment.Models
         public string Code { get; set; }
         public string ReturnUrl { get; set; }
 
-        [Display(Name = "Remember this browser?")]
+        [Display(Name = "Do not Remember this browser?")]
         public bool RememberBrowser { get; set; }
 
         public bool RememberMe { get; set; }
